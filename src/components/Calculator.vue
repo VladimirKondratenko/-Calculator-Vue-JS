5085c59994781424748527377e78a3c409a9d1b9
Vladimir <wolfy_2008@mail.ru>
2020-06-27 14:21:28 +0300

    Add interface

---
+++ src/components/Calculator.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="calculator">
+    <div class="display">{{current || '0'}}</div>
+    <div @click="clear" class="btn">C</div>
+    <div @click="sign" class="btn">+/-</div>
+    <div @click="percent" class="btn">%</div>
+    <div class="btn operator">÷</div>
+    <div class="btn">7</div>
+    <div class="btn">8</div>
+    <div class="btn">9</div>
+    <div class="btn operator">x</div>
+    <div class="btn">4</div>
+    <div class="btn">5</div>
+    <div class="btn">6</div>
+    <div class="btn operator">-</div>
+    <div class="btn">1</div>
+    <div class="btn">2</div>
+    <div class="btn">3</div>
+    <div class="btn operator">+</div>
+    <div class="btn zero">0</div>
+    <div class="btn">.</div>
+    <div class="btn operator">=</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      current: '123',
+    }
+  },
+  methods: {
+    clear() {
+      this.current = '';
+    },
+    sign() {
+      this.current = this.current.charAt(0) === '-' ?
+        this.current.slice(1) : `-${this.current}`;
+    },
+    percent() {
+      this.current = `${parseFloat(this.current) / 100}`;
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .calculator {
+    margin: 0 auto;
+    width: 500px;
+    font-size: 40px;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: minmax(50px, auto);
+  }
+
+  .display {
+    grid-column: 1 / 5;
+    background-color: black;
+    color: white;
+  }
+
+  .zero {
+    grid-column: 1 / 3;
+  }
+
+  .btn {
+    background-color: #F2F2F2;
+    border: 1px solid #999;
+  }
+
+  .operator {
+    color: white;
+    background-color: orange;
+  }
+</style>
